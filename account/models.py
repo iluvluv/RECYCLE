1488172437013d2aeaabce073c9c1718086d2847
ningpop <craft1933@naver.com>
2020-10-31 17:26:23 +0900

    [ningpop] add account models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 15)
