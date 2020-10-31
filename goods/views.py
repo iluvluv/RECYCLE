719ce885a737a797b917b8798085fe08e0c802c8
ningpop <craft1933@naver.com>
2020-10-31 22:11:33 +0900

    [ningpop] add Goods view

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from account.models import *
 from purchase.models import *
 from .models import *
@@ -6,17 +6,16 @@ from .models import *
 
 # 메인 전체리스트
 def index(request):
-    return render(request, "index.html")
-
-# 상품 만들기
-def goods_create(request):
-    return render(request, "goods_create.hrml")
+    kinds_list = Goods.objects.all
+    return render(request, "index.html", kinds_list)
 
 # 종류별 리스트
-def kinds_list(request):
-    return render(request, "kinds_list.html")
+def kinds_list(request, kinds):
+    kinds_list = Goods.objects.filter(kinds=kinds)
+    return render(request, "kinds_list.html", kinds_list)
 
 #상세보기
-def detail(request):
-    return render(request, "detail.html")
+def detail(request, goods_id):
+    goods = get_object_or_404(Goods, pk=goods_id)
+    return render(request, "detail.html", goods)
 
